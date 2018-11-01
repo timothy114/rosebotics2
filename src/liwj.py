a7860af304d8286bfbc390cfeadc80faaf7ff141
timothy114 <liwj@rose-hulman.edu>
2018-11-01 09:14:25 -0400

    Merge branch 'master' of https://github.com/csse120/rosebotics2
    # Conflicts:
    #	src/low_level_rosebotics.py
    #	src/m0.py
    #	src/rosebotics.py

--- src/liwj.py
+++ src/liwj.py
@@ -3,7 +3,7 @@
   Fall term, 2018-2019.
 """
 
-import rosebotics as rb
+import rosebotics_new as rb
 import time
 
 #Making the polygon one
@@ -24,13 +24,28 @@ def polygon(robot,num,length,speed=100):
         robot.drive_system.go_straight_inches(length,speed)
         robot.drive_system.spin_in_place_degrees((180-angle),speed)
 
+def beep_when_hand(robot):
+    #This is for the part where when you stick your hand in front of the robot it stops and beeps
+
+    drive = rb.DriveSystem
+    drive.start_moving(50,50)
+    sensor = rb.InfraredAsProximitySensor()
+    dist = sensor.get_distance_to_nearest_object_in_inches()
+    if dist <= 12:
+        drive.stop_moving()
+        print("Object detected that is less than 12 inches away")
+
+
 def main():
     """ Runs YOUR specific part of the project """
     robot = rb.Snatch3rRobot()
-    polygon(robot,5,20)
-    time.sleep(2)
-    polygon(robot,3,20)
-    time.sleep(2)
-    polygon(robot,6,15)
+
+    #polygon(robot,5,20)
+    #time.sleep(2)
+    #polygon(robot,3,20)
+    #time.sleep(2)
+    #polygon(robot,6,15)
+
+    beep_when_hand(robot)
 
 main()
