585c0d747fabf0d2668e534e29519e11ef679ba4
thuerje <thuerje@rose-hulman.edu>
2018-11-04 19:08:18 -0500

    updated the follow_line definition

--- src/thuerje.py
+++ src/thuerje.py
@@ -8,17 +8,18 @@ import time
 
 def follow_line(robot):
     while True:
-        spin = 30
-        robot.color_sensor.wait_until_intensity_is_greater_than(95)
-        robot.drive_system.start_moving()
-        robot.color_sensor.wait_until_intensity_is_less_than(95)
-        robot.drive_system.stop_moving()
-        for k in range(360):
-            if spin < 360:
-                robot.drive_system.spin_in_place_degrees(spin)
-                if robot.color_sensor.get_reflected_intensity() >= (95):
-                    spin = spin + 360
-                spin = spin * (-k+2)
+        while robot.touch_sensor.get_value() == 0:
+            spin = 30
+            robot.color_sensor.wait_until_intensity_is_greater_than(95)
+            robot.drive_system.start_moving()
+            robot.color_sensor.wait_until_intensity_is_less_than(95)
+            robot.drive_system.stop_moving()
+            for k in range(360):
+                if spin < 360:
+                    robot.drive_system.spin_in_place_degrees(spin)
+                    if robot.color_sensor.get_reflected_intensity() >= (95):
+                        spin = spin + 360
+                    spin = spin * (-k+2)
         if robot.touch_sensor.wait_until_pressed():
             break
 
