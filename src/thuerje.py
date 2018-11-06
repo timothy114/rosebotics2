df1a06c32351e8e5145c99a0573d049752231919
thuerje <thuerje@rose-hulman.edu>
2018-11-06 00:49:44 -0500

    updated thuerje.py

--- src/thuerje.py
+++ src/thuerje.py
@@ -30,202 +30,387 @@ def follow_line(robot):
 
 def build_your_own_pizza():
     pizza_window = rg.RoseWindow(500, 500, 'Pizza Time!')
+    Pizza = pizza(pizza_window)
     window = tkinter.Tk()
     frame = ttk.Frame(window, padding=25)
     frame.grid()
 
     crust_button = ttk.Button(frame, text='Crust')
-    crust_button['command'] = (lambda: crust_frame(pizza_window))
+    crust_button['command'] = (lambda: Pizza.crust_frame(pizza_window))
     crust_button.grid()
 
     sauce_button = ttk.Button(frame, text='Sauce')
-    sauce_button['command'] = (lambda: sauce_frame(pizza_window))
+    sauce_button['command'] = (lambda: Pizza.sauce_frame(pizza_window))
     sauce_button.grid()
 
     cheese_button = ttk.Button(frame, text='Cheese')
-    cheese_button['command'] = (lambda: cheese_frame(pizza_window))
+    cheese_button['command'] = (lambda: Pizza.cheese_frame(pizza_window))
     cheese_button.grid()
 
     toppings_button = ttk.Button(frame, text='Toppings')
-    toppings_button['command'] = (lambda: toppings_frame(pizza_window))
+    toppings_button['command'] = (lambda: Pizza.toppings_frame(pizza_window))
     toppings_button.grid()
 
     pizza_window.close_on_mouse_click()
     window.mainloop()
 
-def crust_frame(pizza_window):
-    window = tkinter.Tk()
-    frame = ttk.Frame(window, padding = 25)
-    frame.grid()
+class pizza(object):
+    def __init__(self, pizza_window):
+        self.counter = 0
+        self.ratio_number = 0
+        self.pw = pizza_window
 
-    small_button = ttk.Button(frame, text = 'Small (10")')
-    small_button['command'] = (lambda: crust(pizza_window, 1))
-    small_button.grid()
+    def crust_frame(self, pizza_window):
+        window = tkinter.Tk()
+        frame = ttk.Frame(window, padding=25)
+        frame.grid()
 
-    medium_button = ttk.Button(frame, text = 'Medium (13")')
-    medium_button['command'] = (lambda: crust(pizza_window, 2))
-    medium_button.grid()
+        small_button = ttk.Button(frame, text='Small (10")')
+        small_button['command'] = (lambda: self.crust(pizza_window, 1))
+        small_button.grid()
 
-    large_button = ttk.Button(frame, text = 'Large (15")')
-    large_button['command'] = (lambda: crust(pizza_window, 3))
-    large_button.grid()
+        medium_button = ttk.Button(frame, text='Medium (13")')
+        medium_button['command'] = (lambda: self.crust(pizza_window, 2))
+        medium_button.grid()
 
-def sauce_frame(pizza_window):
-    window = tkinter.Tk()
-    frame = ttk.Frame(window, padding = 25)
-    frame.grid()
+        large_button = ttk.Button(frame, text='Large (15")')
+        large_button['command'] = (lambda: self.crust(pizza_window, 3))
+        large_button.grid()
 
-    marinara_button = ttk.Button(frame, text = 'Marinara')
-    marinara_button['command'] = (lambda: sauce(pizza_window, 1))
-    marinara_button.grid()
+    def sauce_frame(self, pizza_window):
+        window = tkinter.Tk()
+        frame = ttk.Frame(window, padding=25)
+        frame.grid()
 
-    barbeque_button = ttk.Button(frame, text = 'Barbeque')
-    barbeque_button['command'] = (lambda: sauce(pizza_window, 2))
-    barbeque_button.grid()
+        marinara_button = ttk.Button(frame, text='Marinara')
+        marinara_button['command'] = (lambda: self.sauce(pizza_window, 1))
+        marinara_button.grid()
 
-    pesto_button = ttk.Button(frame, text = 'Pesto')
-    pesto_button['command'] = (lambda: sauce(pizza_window, 3))
-    pesto_button.grid()
+        barbeque_button = ttk.Button(frame, text='Barbeque')
+        barbeque_button['command'] = (lambda: self.sauce(pizza_window, 2))
+        barbeque_button.grid()
 
-    alfredo_button = ttk.Button(frame, text = 'Alfredo')
-    alfredo_button['command'] = (lambda: sauce(pizza_window, 4))
-    alfredo_button.grid()
+        pesto_button = ttk.Button(frame, text='Pesto')
+        pesto_button['command'] = (lambda: self.sauce(pizza_window, 3))
+        pesto_button.grid()
 
-def cheese_frame(pizza_window):
-    window = tkinter.Tk()
-    frame = ttk.Frame(window, padding = 25)
-    frame.grid()
+        alfredo_button = ttk.Button(frame, text='Alfredo')
+        alfredo_button['command'] = (lambda: self.sauce(pizza_window, 4))
+        alfredo_button.grid()
 
-    mozz_button = ttk.Button(frame, text = 'Mozzarella')
-    mozz_button['command'] = (lambda: cheese(pizza_window, 1))
-    mozz_button.grid()
+    def cheese_frame(self, pizza_window):
+        window = tkinter.Tk()
+        frame = ttk.Frame(window, padding=25)
+        frame.grid()
 
-    ched_button = ttk.Button(frame, text = 'Cheddar')
-    ched_button['command'] = (lambda: cheese(pizza_window, 2))
-    ched_button.grid()
+        mozz_button = ttk.Button(frame, text='Mozzarella')
+        mozz_button['command'] = (lambda: self.cheese(pizza_window, 1))
+        mozz_button.grid()
 
-    blue_button = ttk.Button(frame, text = 'Blue Cheese')
-    blue_button['command'] = (lambda: cheese(pizza_window, 3))
-    blue_button.grid()
+        ched_button = ttk.Button(frame, text='Cheddar')
+        ched_button['command'] = (lambda: self.cheese(pizza_window, 2))
+        ched_button.grid()
 
-    gouda_button = ttk.Button(frame, text = 'Gouda')
-    gouda_button['command'] = (lambda: cheese(pizza_window, 4))
-    gouda_button.grid()
+        blue_button = ttk.Button(frame, text='Blue Cheese')
+        blue_button['command'] = (lambda: self.cheese(pizza_window, 3))
+        blue_button.grid()
 
-    ricotta_button = ttk.Button(frame, text = 'Ricotta')
-    ricotta_button['command'] = (lambda: cheese(pizza_window, 5))
-    ricotta_button.grid()
+        gouda_button = ttk.Button(frame, text='Gouda')
+        gouda_button['command'] = (lambda: self.cheese(pizza_window, 4))
+        gouda_button.grid()
 
-def toppings_frame(pizza_window):
-    window = tkinter.Tk()
-    frame = ttk.Frame(window, padding = 25)
-    frame.grid()
+        ricotta_button = ttk.Button(frame, text='Ricotta')
+        ricotta_button['command'] = (lambda: self.cheese(pizza_window, 5))
+        ricotta_button.grid()
 
-    meats_button = ttk.Button(frame, text = 'Meats')
-    meats_button['command'] = (lambda: meats_frame(pizza_window))
-    meats_button.grid()
+    def toppings_frame(self, pizza_window):
+        window = tkinter.Tk()
+        frame = ttk.Frame(window, padding=25)
+        frame.grid()
 
-    vegetables_button = ttk.Button(frame, text = 'Vegetables')
-    vegetables_button['command'] = (lambda: vegetables_frame(pizza_window))
-    vegetables_button.grid()
+        meats_button = ttk.Button(frame, text='Meats')
+        meats_button['command'] = (lambda: self.meats_frame(pizza_window))
+        meats_button.grid()
 
-def meats_frame(pizza_window):
-    window = tkinter.Tk()
-    frame = ttk.Frame(window, padding = 25)
-    frame.grid()
+        vegetables_button = ttk.Button(frame, text='Vegetables')
+        vegetables_button['command'] = (lambda: self.vegetables_frame(pizza_window))
+        vegetables_button.grid()
 
-    pepperoni_button = ttk.Button(frame, text = 'Pepperoni')
-    pepperoni_button['command'] = (lambda: meats(pizza_window, 1))
-    pepperoni_button.grid()
+    def meats_frame(self, pizza_window):
+        window = tkinter.Tk()
+        frame = ttk.Frame(window, padding=25)
+        frame.grid()
 
-    sausage_button = ttk.Button(frame, text = 'Sausage')
-    sausage_button['command'] = (lambda: meats(pizza_window, 2))
-    sausage_button.grid()
+        pepperoni_button = ttk.Button(frame, text='Pepperoni')
+        pepperoni_button['command'] = (lambda: self.meats(pizza_window, 1))
+        pepperoni_button.grid()
 
-    ham_button = ttk.Button(frame, text = 'Ham')
-    ham_button['command'] = (lambda: meats(pizza_window, 3))
-    ham_button.grid()
+        sausage_button = ttk.Button(frame, text='Sausage')
+        sausage_button['command'] = (lambda: self.meats(pizza_window, 2))
+        sausage_button.grid()
 
-    chicken_button = ttk.Button(frame, text = 'Chicken')
-    chicken_button['command'] = (lambda: meats(pizza_window, 4))
-    chicken_button.grid()
+        ham_button = ttk.Button(frame, text='Ham')
+        ham_button['command'] = (lambda: self.meats(pizza_window, 3))
+        ham_button.grid()
 
-    anchovies_button = ttk.Button(frame, text = 'Anchovies')
-    anchovies_button['command'] = (lambda: meats(pizza_window, 5))
-    anchovies_button.grid()
+        chicken_button = ttk.Button(frame, text='Chicken')
+        chicken_button['command'] = (lambda: self.meats(pizza_window, 4))
+        chicken_button.grid()
 
-    tofu_button = ttk.Button(frame, text = 'Tofu')
-    tofu_button['command'] = (lambda: meats(pizza_window, 6))
-    tofu_button.grid()
+        anchovies_button = ttk.Button(frame, text='Anchovies')
+        anchovies_button['command'] = (lambda: self.meats(pizza_window, 5))
+        anchovies_button.grid()
 
-def vegetables_frame(pizza_window):
-    window = tkinter.Tk()
-    frame = ttk.Frame(window, padding = 25)
-    frame.grid()
+        tofu_button = ttk.Button(frame, text='Tofu')
+        tofu_button['command'] = (lambda: self.meats(pizza_window, 6))
+        tofu_button.grid()
 
-    green_pep_button = ttk.Button(frame, text = 'Green Peppers')
-    green_pep_button['command'] = (lambda: vegetables(pizza_window, 1))
-    green_pep_button.grid()
+    def vegetables_frame(self, pizza_window):
+        window = tkinter.Tk()
+        frame = ttk.Frame(window, padding=25)
+        frame.grid()
 
-    red_pep_button = ttk.Button(frame, text = 'Red Peppers')
-    red_pep_button['command'] = (lambda: vegetables(pizza_window, 2))
-    red_pep_button.grid()
+        green_pep_button = ttk.Button(frame, text='Green Peppers')
+        green_pep_button['command'] = (lambda: self.vegetables(pizza_window, 1))
+        green_pep_button.grid()
 
-    tomatoes_button = ttk.Button(frame, text = 'Tomatoes')
-    tomatoes_button['command'] = (lambda: vegetables(pizza_window, 3))
-    tomatoes_button.grid()
+        red_pep_button = ttk.Button(frame, text='Red Peppers')
+        red_pep_button['command'] = (lambda: self.vegetables(pizza_window, 2))
+        red_pep_button.grid()
 
-    onions_button = ttk.Button(frame, text = 'Onions')
-    onions_button['command'] = (lambda: vegetables(pizza_window, 4))
-    onions_button.grid()
+        tomatoes_button = ttk.Button(frame, text='Tomatoes')
+        tomatoes_button['command'] = (lambda: self.vegetables(pizza_window, 3))
+        tomatoes_button.grid()
 
-    olives_button = ttk.Button(frame, text = 'Olives')
-    olives_button['command'] = (lambda: vegetables(pizza_window, 5))
-    olives_button.grid()
+        onions_button = ttk.Button(frame, text='Onions')
+        onions_button['command'] = (lambda: self.vegetables(pizza_window, 4))
+        onions_button.grid()
 
-    pineapple_button = ttk.Button(frame, text = 'Pineapple')
-    pineapple_button['command'] = (lambda: vegetables(pizza_window, 6))
-    pineapple_button.grid()
+        olives_button = ttk.Button(frame, text='Olives')
+        olives_button['command'] = (lambda: self.vegetables(pizza_window, 5))
+        olives_button.grid()
 
-    spinach_button = ttk.Button(frame, text = 'Spinach')
-    spinach_button['command'] = (lambda: vegetables(pizza_window, 7))
-    spinach_button.grid()
+        pineapple_button = ttk.Button(frame, text='Pineapple')
+        pineapple_button['command'] = (lambda: self.vegetables(pizza_window, 6))
+        pineapple_button.grid()
 
-    mushroom_button = ttk.Button(frame, text = 'Mushrooms')
-    mushroom_button['command'] = (lambda: vegetables(pizza_window, 8))
-    mushroom_button.grid()
+        spinach_button = ttk.Button(frame, text='Spinach')
+        spinach_button['command'] = (lambda: self.vegetables(pizza_window, 7))
+        spinach_button.grid()
 
-def crust(window, value):
-    point = rg.Point(250, 250)
-    radius = 0
-    if value == 1:
-        radius = 100
-    if value == 2:
-        radius = 130
-    if value == 3:
-        radius = 150
-    circle = rg.Circle(point, radius)
-    circle.fill_color = 'tan'
-    circle.attach_to(window)
-    window.render()
+        mushroom_button = ttk.Button(frame, text='Mushrooms')
+        mushroom_button['command'] = (lambda: self.vegetables(pizza_window, 8))
+        mushroom_button.grid()
 
-def sauce(window, value):
-    point = rg.Point(250, 250)
-    circle = rg.Circle(point, 100)
-    if value == 1:
-        circle.fill_color = 'red'
-    if value == 2:
-        circle.fill_color = 'brown'
-    if value == 3:
-        circle.fill_color = 'green'
-    if value == 4:
-        circle.fill_color = 'white'
-    circle.attach_to(window)
-    window.render()
+    def crust(self, window, value):
+        point = rg.Point(250, 250)
+        radius = 0
+        if value == 1:
+            radius = 100
+            self.ratio_number = 100
+        if value == 2:
+            radius = 130
+            self.ratio_number = 130
+        if value == 3:
+            radius = 150
+            self.ratio_number = 150
+        circle = rg.Circle(point, radius)
+        circle.fill_color = 'tan'
+        circle.attach_to(window)
+        window.render()
+
+    def sauce(self, window, value):
+        # if self.ratio_number == 0:
+        #     return
+        point = rg.Point(250, 250)
+        radius = (self.ratio_number) - 10
+        circle = rg.Circle(point, radius)
+        if value == 1:
+            circle.fill_color = 'red'
+        if value == 2:
+            circle.fill_color = 'brown'
+        if value == 3:
+            circle.fill_color = 'green'
+        if value == 4:
+            circle.fill_color = 'white'
+        circle.attach_to(window)
+        window.render()
 
 
 
 
+
+
+
+# def crust_frame(pizza_window):
+#     window = tkinter.Tk()
+#     frame = ttk.Frame(window, padding = 25)
+#     frame.grid()
+#
+#     small_button = ttk.Button(frame, text = 'Small (10")')
+#     small_button['command'] = (lambda: crust(pizza_window, 1))
+#     small_button.grid()
+#
+#     medium_button = ttk.Button(frame, text = 'Medium (13")')
+#     medium_button['command'] = (lambda: crust(pizza_window, 2))
+#     medium_button.grid()
+#
+#     large_button = ttk.Button(frame, text = 'Large (15")')
+#     large_button['command'] = (lambda: crust(pizza_window, 3))
+#     large_button.grid()
+#
+# def sauce_frame(pizza_window):
+#     window = tkinter.Tk()
+#     frame = ttk.Frame(window, padding = 25)
+#     frame.grid()
+#
+#     marinara_button = ttk.Button(frame, text = 'Marinara')
+#     marinara_button['command'] = (lambda: sauce(pizza_window, 1))
+#     marinara_button.grid()
+#
+#     barbeque_button = ttk.Button(frame, text = 'Barbeque')
+#     barbeque_button['command'] = (lambda: sauce(pizza_window, 2))
+#     barbeque_button.grid()
+#
+#     pesto_button = ttk.Button(frame, text = 'Pesto')
+#     pesto_button['command'] = (lambda: sauce(pizza_window, 3))
+#     pesto_button.grid()
+#
+#     alfredo_button = ttk.Button(frame, text = 'Alfredo')
+#     alfredo_button['command'] = (lambda: sauce(pizza_window, 4))
+#     alfredo_button.grid()
+#
+# def cheese_frame(pizza_window):
+#     window = tkinter.Tk()
+#     frame = ttk.Frame(window, padding = 25)
+#     frame.grid()
+#
+#     mozz_button = ttk.Button(frame, text = 'Mozzarella')
+#     mozz_button['command'] = (lambda: cheese(pizza_window, 1))
+#     mozz_button.grid()
+#
+#     ched_button = ttk.Button(frame, text = 'Cheddar')
+#     ched_button['command'] = (lambda: cheese(pizza_window, 2))
+#     ched_button.grid()
+#
+#     blue_button = ttk.Button(frame, text = 'Blue Cheese')
+#     blue_button['command'] = (lambda: cheese(pizza_window, 3))
+#     blue_button.grid()
+#
+#     gouda_button = ttk.Button(frame, text = 'Gouda')
+#     gouda_button['command'] = (lambda: cheese(pizza_window, 4))
+#     gouda_button.grid()
+#
+#     ricotta_button = ttk.Button(frame, text = 'Ricotta')
+#     ricotta_button['command'] = (lambda: cheese(pizza_window, 5))
+#     ricotta_button.grid()
+#
+# def toppings_frame(pizza_window):
+#     window = tkinter.Tk()
+#     frame = ttk.Frame(window, padding = 25)
+#     frame.grid()
+#
+#     meats_button = ttk.Button(frame, text = 'Meats')
+#     meats_button['command'] = (lambda: meats_frame(pizza_window))
+#     meats_button.grid()
+#
+#     vegetables_button = ttk.Button(frame, text = 'Vegetables')
+#     vegetables_button['command'] = (lambda: vegetables_frame(pizza_window))
+#     vegetables_button.grid()
+#
+# def meats_frame(pizza_window):
+#     window = tkinter.Tk()
+#     frame = ttk.Frame(window, padding = 25)
+#     frame.grid()
+#
+#     pepperoni_button = ttk.Button(frame, text = 'Pepperoni')
+#     pepperoni_button['command'] = (lambda: meats(pizza_window, 1))
+#     pepperoni_button.grid()
+#
+#     sausage_button = ttk.Button(frame, text = 'Sausage')
+#     sausage_button['command'] = (lambda: meats(pizza_window, 2))
+#     sausage_button.grid()
+#
+#     ham_button = ttk.Button(frame, text = 'Ham')
+#     ham_button['command'] = (lambda: meats(pizza_window, 3))
+#     ham_button.grid()
+#
+#     chicken_button = ttk.Button(frame, text = 'Chicken')
+#     chicken_button['command'] = (lambda: meats(pizza_window, 4))
+#     chicken_button.grid()
+#
+#     anchovies_button = ttk.Button(frame, text = 'Anchovies')
+#     anchovies_button['command'] = (lambda: meats(pizza_window, 5))
+#     anchovies_button.grid()
+#
+#     tofu_button = ttk.Button(frame, text = 'Tofu')
+#     tofu_button['command'] = (lambda: meats(pizza_window, 6))
+#     tofu_button.grid()
+#
+# def vegetables_frame(pizza_window):
+#     window = tkinter.Tk()
+#     frame = ttk.Frame(window, padding = 25)
+#     frame.grid()
+#
+#     green_pep_button = ttk.Button(frame, text = 'Green Peppers')
+#     green_pep_button['command'] = (lambda: vegetables(pizza_window, 1))
+#     green_pep_button.grid()
+#
+#     red_pep_button = ttk.Button(frame, text = 'Red Peppers')
+#     red_pep_button['command'] = (lambda: vegetables(pizza_window, 2))
+#     red_pep_button.grid()
+#
+#     tomatoes_button = ttk.Button(frame, text = 'Tomatoes')
+#     tomatoes_button['command'] = (lambda: vegetables(pizza_window, 3))
+#     tomatoes_button.grid()
+#
+#     onions_button = ttk.Button(frame, text = 'Onions')
+#     onions_button['command'] = (lambda: vegetables(pizza_window, 4))
+#     onions_button.grid()
+#
+#     olives_button = ttk.Button(frame, text = 'Olives')
+#     olives_button['command'] = (lambda: vegetables(pizza_window, 5))
+#     olives_button.grid()
+#
+#     pineapple_button = ttk.Button(frame, text = 'Pineapple')
+#     pineapple_button['command'] = (lambda: vegetables(pizza_window, 6))
+#     pineapple_button.grid()
+#
+#     spinach_button = ttk.Button(frame, text = 'Spinach')
+#     spinach_button['command'] = (lambda: vegetables(pizza_window, 7))
+#     spinach_button.grid()
+#
+#     mushroom_button = ttk.Button(frame, text = 'Mushrooms')
+#     mushroom_button['command'] = (lambda: vegetables(pizza_window, 8))
+#     mushroom_button.grid()
+#
+# def crust(window, value):
+#     point = rg.Point(250, 250)
+#     radius = 0
+#     if value == 1:
+#         radius = 100
+#     if value == 2:
+#         radius = 130
+#     if value == 3:
+#         radius = 150
+#     circle = rg.Circle(point, radius)
+#     circle.fill_color = 'tan'
+#     circle.attach_to(window)
+#     window.render()
+#
+# def sauce(window, value):
+#     point = rg.Point(250, 250)
+#     circle = rg.Circle(point, 100)
+#     if value == 1:
+#         circle.fill_color = 'red'
+#     if value == 2:
+#         circle.fill_color = 'brown'
+#     if value == 3:
+#         circle.fill_color = 'green'
+#     if value == 4:
+#         circle.fill_color = 'white'
+#     circle.attach_to(window)
+#     window.render()
 
 
 
