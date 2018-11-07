7ea111158d89d980a813a0e7b8559e4e37e944fa
thuerje <thuerje@rose-hulman.edu>
2018-11-07 09:30:27 -0500

    updated rosebotics_new

--- src/capstone_3_runs_on_robot.py
+++ src/capstone_3_runs_on_robot.py
@@ -7,16 +7,16 @@ Also: responds to Beacon button-presses by beeping, speaking.
 This module runs on the ROBOT.
 It uses MQTT to RECEIVE information from a program running on the LAPTOP.
 
-Authors:  David Mutchler, his colleagues, and PUT_YOUR_NAME_HERE.
+Authors:  David Mutchler, his colleagues, and Jess Thuer
 """
 # ------------------------------------------------------------------------------
-# TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.  Then delete this TODO.
+# DONE 1. PUT YOUR NAME IN THE ABOVE LINE.  Then delete this.
 # ------------------------------------------------------------------------------
 
 # ------------------------------------------------------------------------------
-# TODO: 2. With your instructor, review the "big picture" of laptop-robot
-# TODO:    communication, per the comment in mqtt_sender.py.
-# TODO:    Once you understand the "big picture", delete this TODO.
+# DONE 2. With your instructor, review the "big picture" of laptop-robot
+#     communication, per the comment in mqtt_sender.py.
+#     Once you understand the "big picture", delete this.
 # ------------------------------------------------------------------------------
 
 import rosebotics_new as rb
@@ -27,36 +27,55 @@ import ev3dev.ev3 as ev3
 
 def main():
     # --------------------------------------------------------------------------
-    # TODO: 3. Construct a Snatch3rRobot.  Test.  When OK, delete this TODO.
+    # DONE 3. Construct a Snatch3rRobot.  Test.  When OK, delete this.
+    # --------------------------------------------------------------------------
+    robot = rb.Snatch3rRobot()
+
+    # --------------------------------------------------------------------------
+    # DONE 4. Add code that constructs a   com.MqttClient   that will
+    #     be used to receive commands sent by the laptop.
+    #     Connect it to this robot.  Test.  When OK, delete this.
+    # --------------------------------------------------------------------------
+    rc = RemoteControlEtc(robot)
+    mqtt_client = com.MqttClient(rc)
+    mqtt_client.connect_to_pc()
+
+    # --------------------------------------------------------------------------
+    # DONE 5. Add a class for your "delegate" object that will handle messages
+    #     sent from the laptop.  Construct an instance of the class and
+    #     pass it to the MqttClient constructor above.  Augment the class
+    #     as needed for that, and also to handle the go_forward message.
+    #     Test by PRINTING, then with robot.  When OK, delete this.
     # --------------------------------------------------------------------------
 
     # --------------------------------------------------------------------------
-    # TODO: 4. Add code that constructs a   com.MqttClient   that will
-    # TODO:    be used to receive commands sent by the laptop.
-    # TODO:    Connect it to this robot.  Test.  When OK, delete this TODO.
-    # --------------------------------------------------------------------------
-
-    # --------------------------------------------------------------------------
-    # TODO: 5. Add a class for your "delegate" object that will handle messages
-    # TODO:    sent from the laptop.  Construct an instance of the class and
-    # TODO:    pass it to the MqttClient constructor above.  Augment the class
-    # TODO:    as needed for that, and also to handle the go_forward message.
-    # TODO:    Test by PRINTING, then with robot.  When OK, delete this TODO.
-    # --------------------------------------------------------------------------
-
-    # --------------------------------------------------------------------------
-    # TODO: 6. With your instructor, discuss why the following WHILE loop,
-    # TODO:    that appears to do nothing, is necessary.
-    # TODO:    When you understand this, delete this TODO.
+    # DONE 6. With your instructor, discuss why the following WHILE loop,
+    #     that appears to do nothing, is necessary.
+    #     When you understand this, delete this.
     # --------------------------------------------------------------------------
     while True:
         # ----------------------------------------------------------------------
         # TODO: 7. Add code that makes the robot beep if the top-red button
-        # TODO:    on the Beacon is pressed.  Add code that makes the robot
-        # TODO:    speak "Hello. How are you?" if the top-blue button on the
-        # TODO:    Beacon is pressed.  Test.  When done, delete this TODO.
+        #     on the Beacon is pressed.  Add code that makes the robot
+        #     speak "Hello. How are you?" if the top-blue button on the
+        #     Beacon is pressed.  Test.  When done, delete this.
         # ----------------------------------------------------------------------
         time.sleep(0.01)  # For the delegate to do its work
+
+
+
+class RemoteControlEtc(object):
+    def __init__(self, robot):
+        self.robot = robot
+        """
+        Stores a robot.
+            :type robot: rb.Snatch3rRobot
+        """
+
+    def go_forward(self, speed_string):
+        speed = int(speed_string)
+        print('Robot should start moving.')
+        self.robot.drive_system.start_moving(speed, speed)
 
 
 main()
