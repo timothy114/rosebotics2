e23b9222db54f3d698f69d05211dbdeaf1651b5f
Toluwa Nafiu <nafiutc@rose-hulman.edu>
2018-11-07 09:19:20 -0500

    Finished mqtt sender code for robot

--- src/capstone_1_runs_on_robot.py
+++ src/capstone_1_runs_on_robot.py
@@ -23,29 +23,16 @@ import ev3dev.ev3 as ev3
 
 
 def main():
-    # --------------------------------------------------------------------------
-    # TODO: 3. Construct a Snatch3rRobot.  Test.  When OK, delete this TODO.
-    # --------------------------------------------------------------------------
+    robot = rb.Snatch3rRobot()
 
     # --------------------------------------------------------------------------
     # TODO: 4. Add code that constructs a   com.MqttClient   that will
     # TODO:    be used to receive commands sent by the laptop.
     # TODO:    Connect it to this robot.  Test.  When OK, delete this TODO.
     # --------------------------------------------------------------------------
-
-    # --------------------------------------------------------------------------
-    # TODO: 5. Add a class for your "delegate" object that will handle messages
-    # TODO:    sent from the laptop.  Construct an instance of the class and
-    # TODO:    pass it to the MqttClient constructor above.  Augment the class
-    # TODO:    as needed for that, and also to handle the go_forward message.
-    # TODO:    Test by PRINTING, then with robot.  When OK, delete this TODO.
-    # --------------------------------------------------------------------------
-
-    # --------------------------------------------------------------------------
-    # TODO: 6. With your instructor, discuss why the following WHILE loop,
-    # TODO:    that appears to do nothing, is necessary.
-    # TODO:    When you understand this, delete this TODO.
-    # --------------------------------------------------------------------------
+    rc = RemoteControlEtc(robot)
+    mqtt_client = com.MqttClient(rc)
+    mqtt_client.connect_to_pc()
     while True:
         # ----------------------------------------------------------------------
         # TODO: 7. Add code that makes the robot beep if the top-red button
@@ -54,6 +41,21 @@ def main():
         # TODO:    Beacon is pressed.  Test.  When done, delete this TODO.
         # ----------------------------------------------------------------------
         time.sleep(0.01)  # For the delegate to do its work
+
+
+class RemoteControlEtc(object):
+
+    def __init__(self, robot):
+        """
+        Stores a robot.
+            :type robot: rb.Snatch3rRobot
+        """
+        self.robot = robot
+
+    def go_forward(self, speed_string):
+        speed = input(speed_string)
+        print('Robot should statrt moving')
+        self.robot.drive_system.start_moving(speed, speed)
 
 
 main()
