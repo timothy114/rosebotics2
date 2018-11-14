39e6c42706693810d073df0555177fac98bb552a
Toluwa Nafiu <nafiutc@rose-hulman.edu>
2018-11-14 00:04:45 -0500

    Adding PID control and images

--- src/nafiu_final_runs_on_laptop.py
+++ src/nafiu_final_runs_on_laptop.py
@@ -1,38 +1,43 @@
 import tkinter
 from tkinter import ttk
+from PIL import ImageTk, Image
 import mqtt_remote_method_calls as com
 
 
 def main():
     """ Constructs and runs a GUI for this program. """
     root = tkinter.Tk()
+    root.title('Ready for user input!')
+    root.geometry('300x400')
 
     mqtt_client = com.MqttClient()
     mqtt_client.connect_to_ev3()
 
-    setup_gui(root, mqtt_client)
+    setup_gui_line_following(root, mqtt_client)
+    setup_gui_remote_control(root, mqtt_client)
+    # handle_found_home()
 
     root.mainloop()
 
 
-def setup_gui(root_window, mqtt_client):
+def setup_gui_line_following(root_window, mqtt_client):
     """ Constructs and sets up widgets on the given window. """
     frame = ttk.Frame(root_window, padding=10)
     frame.grid()
+    style1 = ttk.Style()
+    style1.configure("TButton", foreground="green", background="red")
 
     speed_entry_box = ttk.Entry(frame)
     color_entry_box = ttk.Entry(frame)
-    speed_button = ttk.Button(frame, text="Enter speed")
+    speed_button = ttk.Button(frame, text="Enter speed", style="TButton")
     follow_color_button = ttk.Button(frame, text="Enter color")
-    beep_button = ttk.Button(frame, text="Beep")
-    greet_button = ttk.Button(frame, text="Greeting")
+
 
     speed_entry_box.grid()
     speed_button.grid()
     color_entry_box.grid()
     follow_color_button.grid()
-    beep_button.grid()
-    greet_button.grid()
+
     """
     How do I send BOTH the color and the speed data to 'handle_find_line'? I want 'handle_find_line' to receive the speed
     value when the speed button is pressed and the color value when the color is pressed but I don't want it to run the 
@@ -44,6 +49,27 @@ def setup_gui(root_window, mqtt_client):
         lambda: handle_find_line(speed_entry_box,color_entry_box, mqtt_client)
 
 
+def setup_gui_remote_control(root_window, mqtt_client):
+    frame = ttk.Frame(root_window, padding=10)
+    frame.grid()
+
+    beep_button = ttk.Button(frame, text="Beep")
+    greet_button = ttk.Button(frame, text="Greeting")
+    beep_button.grid()
+    greet_button.grid()
+
+    beep_button['command'] = lambda: handle_robot_beep(mqtt_client)
+    greet_button['command'] = lambda: handle_robot_greeting(mqtt_client)
+
+
+def handle_robot_beep(mqtt_client):
+    mqtt_client.send_message('robot_beep', [])
+
+
+def handle_robot_greeting(mqtt_client):
+    mqtt_client.send_message('robot_greeting', [])
+
+
 def handle_find_line(entry_box1, entry_box2, mqtt_client):
     """
     Tells the robot to go forward at the speed specified in the given entry box and to find the starting point with the
@@ -53,6 +79,29 @@ def handle_find_line(entry_box1, entry_box2, mqtt_client):
     color = entry_box2.get()
     print("Sending 'find_line' to the robot with a speed of", speed, "and a color of", color)
     mqtt_client.send_message('find_line', [speed, color])
+
+
+def handle_found_home():
+    # constructs the window for the final "home" screen
+    window = tkinter.Tk()
+    window.configure(background='yellow')
+    window.title("Home!")
+    window.geometry("300x300")
+    window.configure(background='grey')
+
+    path = "welcome-home.jpg"
+
+    # makes a Tkinter photo image
+    image = tkinter.PhotoImage(file=path)
+
+    # The Label widget is used to display the image on the window
+    label = ttk.Label(window, image=image)
+
+    # The Pack manger packs the image and any text together
+    label.pack(side="bottom", fill="both", expand="yes")
+
+    # used to start the GUI running
+    window.mainloop()
 
 
 main()
--- src/nafiutc_final_runs_on_robot.py
+++ src/nafiutc_final_runs_on_robot.py
@@ -9,6 +9,7 @@ def main():
     rc = RemoteControlEtc(robot)
     mqtt_client = com.MqttClient(rc)
     mqtt_client.connect_to_pc()
+    mqtt_client.find_home()
     while True:
         time.sleep(0.01)  # Gives time for the delegate to do its work
         if robot.beacon_button_sensor.is_top_red_button_pressed():
@@ -26,18 +27,53 @@ class RemoteControlEtc(object):
         """
         self.robot = robot
 
+    def robot_beep(self):
+        ev3.Sound.beep(1).wait()
+
+    def robot_greeting(self):
+        ev3.Sound.speak('Hi, how are you?')
+
     def find_line(self, speed_string, color_string):
         speed = int(speed_string)
         color = int(color_string)
         print('Robot should start moving')
         self.robot.drive_system.start_moving(speed, speed)
+        while True:
+            if self.robot.color_sensor.get_color() == color:
+                self.robot.drive_system.stop_moving()
+                break
+            else:
+                while self.robot.touch_sensor.get_value() == 0:
+                    spin = 1
+                    self.robot.color_sensor.wait_until_intensity_is_less_than(5)
+                    self.robot.drive_system.start_moving()
+                    self.robot.color_sensor.wait_until_intensity_is_greater_than(5)
+                    self.robot.drive_system.stop_moving()
+                    for k in range(360):
+                        if spin < 360:
+                            self.robot.drive_system.spin_in_place_degrees(spin)
+                            if self.robot.color_sensor.get_reflected_intensity() <= (5):
+                                spin = spin + 360
+                            spin = -spin * (-k + 2)
 
-    def find_color(self, color_string):
-        color = int(color_string)
-        robot = rb.Snatch3rRobot()
-        robot.drive_system.start_moving(50, 50)
-        print(color)
-        if robot.color_sensor.get_color() == color:
-            robot.drive_system.stop_moving()
+    def find_home(self, mqtt_client):
+        while True:
+            while self.robot.touch_sensor.get_value() == 0:
+                spin = 1
+                self.robot.color_sensor.wait_until_intensity_is_less_than(5)
+                self.robot.drive_system.start_moving()
+                self.robot.color_sensor.wait_until_intensity_is_greater_than(5)
+                self.robot.drive_system.stop_moving()
+                for k in range(360):
+                    if spin < 360:
+                        self.robot.drive_system.spin_in_place_degrees(spin)
+                        if self.robot.color_sensor.get_reflected_intensity() <= (5):
+                            spin = spin + 360
+                        spin = -spin * (-k + 2)
+            if self.robot.camera:
+                time.sleep(0.1)
+                # mqtt_client.send_message('handle_found_home', [])
+                break
+
 
 main()
